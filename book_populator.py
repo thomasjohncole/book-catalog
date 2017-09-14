185b00e817145447bdee92cdc08fd512d1217408
Tom Cole <colecode.ph@gmail.com>
2017-09-14 17:20:23 +0800

    Add a couple more entries to the db via the populator file

--- book_populator.py
+++ book_populator.py
@@ -23,4 +23,18 @@ book1 = Book(title = "Excession",
              category_id = "1")
 session.add(book1)
 
+book2 = Book(title = "The Dosadai Experiment",
+             author = "Frank Herbert",
+             description = "A secret experiment on a secret planet gets way out of hand.",
+             category_id = "1")
+session.add(book2)
+
+book3 = Book(title = "Starfish",
+             author = "Peter Watts",
+             description = "Modified humans living in the deep sea experience ganzfeld effects.",
+             category_id = "1")
+session.add(book3)
+
+
+
 session.commit()
