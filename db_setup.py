4753484874cd77a07e58efd4df6a889bc54540de
Tom Cole <colecode.ph@gmail.com>
2017-10-04 15:14:22 +0800

    Add user table to db, and user_id column to category and book table

--- db_setup.py
+++ db_setup.py
@@ -6,12 +6,24 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
-# creates two tables with the specified columns #
+# creates three tables with the specified columns #
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
 class Category(Base):
     __tablename__ = 'category'
 
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
 
 class Book(Base):
     __tablename__ = 'book'
@@ -24,6 +36,8 @@ class Book(Base):
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
